8f0bb8eff1274c04187496b4f7be6c2ca154ab57
nams019 <61611128+nams019@users.noreply.github.com>
2022-07-31 16:00:16 +0530

    Add files via upload

---
+++ Github.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+os.getcwd()
+
+
+# In[4]:
+
+
+import pandas as pd
+pd.read_csv("Data.tsv",sep="\t")
+
+
+# In[7]:
+
+
+import pandas as pd
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/dataset1/master/Skyserver.csv")
+
+
+# In[ ]:
+
+
+
+
